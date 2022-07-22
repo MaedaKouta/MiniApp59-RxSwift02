d3277043ebdb6236b68a9cf8d0bae682913313cd
Kota <maedakoutaa@gmail.com>
2022-07-22 09:44:37 +0900

    commit

--- MiniApp59-RxSwift02/ViewController.swift
+++ MiniApp59-RxSwift02/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var textField: UITextField!
+    @IBOutlet private weak var textLabel: UILabel!
+    private let minCharacterSize = 400
+    let disposeBag = DisposeBag()
+
+    let limitText: (Int) -> String = {
+        return "残り\($0)文字"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.rx.text.orEmpty
+            .map{ [weak self] text -> String? in
+                let limitCount = self!.minCharacterSize - text.count
+                return self?.limitText(limitCount)
+            }
+            .bind(to: textLabel.rx.text)
+            .disposed(by: disposeBag)
     }
 
 
